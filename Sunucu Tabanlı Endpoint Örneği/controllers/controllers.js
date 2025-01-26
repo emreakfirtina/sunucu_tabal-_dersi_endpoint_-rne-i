b36094d13efd546138071df52edb51dd90858246
emreakfirtina <160278905+emreakfirtina@users.noreply.github.com>
2025-01-26 15:59:43 +0300

    endpoint örneği

--- Sunucu Tabanlı Endpoint Örneği/controllers/controllers.js
+++ Sunucu Tabanlı Endpoint Örneği/controllers/controllers.js
@@ -8,3 +8,26 @@ const urun_getir = (req, res) => {
         }
     })
 }
+
+const satis_getir_by_tarih=async(req,res)=>{
+    const {startDate,endDate}=req.query
+    if(!startDate||!endDate){
+        return res.status(400).json({
+            success:false,
+            message:"Başlangıç ve Bitiş tarihleri eksik"
+        })
+    }
+    try{
+        const [results]=await dbConn.query("SELECT * FROM satislar Where siparis_tarihi BETWEEN ? AND ?",[startDate,endDate])
+        return res.status(200).json({
+            success:true,
+            data:results
+        })
+    }catch(error){
+        return res.status(500).json({
+            success:false,
+            message:"Veriler Alınamadı"
+        })
+    }
+    }
+    module.exports={urun_getir,siparis_getir_by_tarih}
